eb45fc8c73acd15f41441a7ec00333a955d31380
Peter Maunz <peter@maunz.name>
2017-12-03 23:19:31 -0700

    add CIntMin and CIntMax

--- use_int32.go
+++ use_int32.go
@@ -6,3 +6,5 @@ type CInt int32
 
 const loRange CInt = 46340
 const hiRange CInt = 46340
+const CIntMin = CInt(-0x80000000)
+const CIntMax = CInt(0x7FFFFFFF)
--- use_int64.go
+++ use_int64.go
@@ -6,3 +6,5 @@ type CInt int64
 
 const loRange CInt = 0x3FFFFFFF
 const hiRange CInt = 0x3FFFFFFFFFFFFFFF //L;  // it won't compile with the L there
+const CIntMin = CInt(-0x8000000000000000)
+const CIntMax = CInt(0x7FFFFFFFFFFFFFFF)
